6224de7cdcfcdf6bc3ed6a0e6fc8d675ba3ee6be
KhalenH <35700252+KhalenH@users.noreply.github.com>
2018-05-07 17:53:21 -0400

    More learning

---
+++ Assets/Scipts/S0_1PreFab.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class S0_1PreFab : MonoBehaviour
+{
+
+    public int worldSizeW = 64; // x-direction
+    public int worldSizeH = 64;  // z-direction
+
+    public GameObject prefabGrass;
+
+    void Start()
+    {
+        int y = 0;
+        for (int z = 0; z < worldSizeH; z++)
+        {
+            for (int x = 0; x < worldSizeW; x++)
+            {
+                Vector3 pos = new Vector3(x, y, z);
+                Instantiate(prefabGrass, pos, Quaternion.identity);
+            }
+        }
+    }
+
+}
